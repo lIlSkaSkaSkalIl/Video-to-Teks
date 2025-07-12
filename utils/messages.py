d3cc37244aa05eb634830b4b6eb8a07656bb1d0a
lIlSkaSkaSkalIl <168249736+lIlSkaSkaSkalIl@users.noreply.github.com>
2025-07-12 14:48:10 +0700

    Update messages.py

--- utils/messages.py
+++ utils/messages.py
@@ -5,7 +5,7 @@ def log(msg, icon="📌"):
     
 def folder_structure_status(video_dir, audio_dir, output_dir, log_dir):
     print("📂 Folder yang dibuat:")
-    print(f"   ├─🎞️ Video : {video_dir}")
+    print(f"   ┌─🎞️ Video : {video_dir}")
     print(f"   ├─🎧 Audio : {audio_dir}")
     print(f"   ├─📜 Output: {output_dir}")
     print(f"   └─🧾 Log   : {log_dir}")
